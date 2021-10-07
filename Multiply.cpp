222a4cc55a1ce8c3247c8b2a8e75b8dd2ef9154c
Apoorv Priyadarsh <apoorvpriyadarsh1729@gmail.com>
2021-10-07 16:50:08 +0530

    Create Multiply.cpp
    Multiply two numbers.

---
+++ Multiply.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    productOfTwoNumbers = firstNumber*secondNumber;
+ 
+    // Prints product 
+    cout << firstNumber << " + " <<  secondNumber << " = " << productOfTwoNumbers;     
+   
+
+    
+
+    return 0;
+}
